d1132dc7f6688d9439c4d98fe5702a4ea4610256
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-21 23:11:38 +0530

    completed the storage.py

---
+++ src/storage.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+FILE_PATH = "data/storage.json"
+
+def load_data():
+    if not os.path.exists(FILE_PATH):
+        return []
+    
+    try:
+        with open(FILE_PATH, "r") as file:
+            content = file.read().strip()
+
+            if content == "":
+                return []
+            return json.loads(content)
+        
+    except:
+        return []
+    
+def save_data(data):
+    with open(FILE_PATH, "W") as file:
+        json.dump(data, file, indent=4)
