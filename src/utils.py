e53fde053bdb66b241e0ccf690584a19578f4591
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-22 09:22:41 +0530

    corrected utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ def generate_id(records):
         return 1
     return records[-1]["id"] + 1
 
-def is_valud_number(value):
+def is_valid_number(value):
     try:
         int(value)
         return True
