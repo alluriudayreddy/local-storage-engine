e6984bf1529c5410c15b0c1c35893772492776ad
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-23 14:03:27 +0530

    added created time filed to records(when a record is created, save the date/time too)

--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 def create_record(record_id, name, value):
     return{
         "id": record_id,
         "name": name,
-        "value": value
+        "value": value,
+        "created_at": datetime.now().strftime("%Y-%m-%d %H-%M-%S")
     }
