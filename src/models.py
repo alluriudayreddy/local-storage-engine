817d8524394c520362811387c39d2d25dec7b520
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-23 15:44:59 +0530

    changed the domain in models.py (name, price, quantity, category, brand, status)

--- src/models.py
+++ src/models.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
-def create_record(record_id, name, value):
+def create_record(record_id, name, price, quantity, category, brand, status):
     return{
         "id": record_id,
         "name": name,
-        "value": value,
+        "price": price,
+        "quantity": quantity,
+        "category": category,
+        "brand": brand,
+        "status": status,
         "created_at": datetime.now().strftime("%Y-%m-%d %H-%M-%S")
     }
