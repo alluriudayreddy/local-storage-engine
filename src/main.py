d53b3d8fb49e7aa1ee122659b77f0ab51ee4c837
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-23 11:27:47 +0530

    added search by ID, name, and confirmation before delete the record in main.py

--- src/main.py
+++ src/main.py
@@ -9,7 +9,9 @@ while True:
     print("2. View Records")
     print("3. Update Record")
     print("4. Delete Record")
-    print("5. Exit")
+    print("5. Search by ID")
+    print("6. Search by Name")
+    print("7. Exit")
     print("------------------------")
 
 
@@ -69,22 +71,73 @@ while True:
         record_id  = input("Enter record ID to delete: ")
         
         if is_valid_number(record_id):
-            deleted = remove_record(int(record_id))
+            confirm = input("Are you sure you want to delete? (Y/n):")
+            while confirm != "Y" and confirm != "n":
+                print("Invalid option. Please enter Y or n.")
+                confirm = input("Are you sure you want to delete? (Y/n): ")
+                
+            if confirm == "Y":
+                deleted = remove_record(int(record_id))
 
-            if deleted:
-                print("Record deleted.")
+                if deleted:
+                    print("Record deleted.")
+                else:
+                    print("Record not found to delete. Please check the ID and try again.")
             else:
-                print("Record not found to delete. Please check the ID and try again.")
-        else:
-            print("Invalid ID. Please enter a valid number.")
+                print("Invalid ID. Please enter a valid number.")
 
-
+ 
 
 
     elif choice == "5":
+        record_id =  input("Enter record ID to search: ")
+
+        if is_valid_number(record_id):
+            record_list = get_all_record_list()
+            found = False
+
+            for record_item in record_list:
+                if record_item["id"] == int(record_id):
+                    print("\nID | NAME | VALUE")
+                    print("--------------------")
+                    print(f'{record_item["id"]} | {record_item["name"]} | {record_item["value"]}')
+                    found =True
+                    break
+
+            if not found:
+                print("Record not found. Please check the ID and try again.")
+            else:
+                print("Invalid ID. Please enter a valid number.")
+
+
+
+
+    elif choice == "6":
+        search_name = input("Enter name: ").strip()
+
+        if search_name == "":
+            print("Name cannot be empty. Please enter a valid name.")
+        else:
+            record_list = get_all_record_list()
+            found = False
+
+            print("\nID | NAME | VALUE")
+            print("--------------------")
+
+            for record_item in record_list:
+                if record_item["name"].lower() == search_name.lower():
+                    print(f'{record_item["id"]} | {record_item["name"]} | {record_item["value"]}')
+                    found =True
+
+            if not found:
+                print("No records found with the given name. Please check the name and try again.")
+
+
+
+    elif choice == "7":
         print("Going to Exit the system....")
         break
 
     else:
-        print("Invalid choice. Choose 1-5.")
+        print("Invalid choice. Choose 1-7.")
         
