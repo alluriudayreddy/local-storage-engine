bafd1a9060dec6f883da1cc93612a8c2e4924bcf
Alluri Uday Reddy <alluriudayreddy16@gmail.com>
2026-04-23 15:49:50 +0530

    modified engine.py

--- src/engine.py
+++ src/engine.py
@@ -1,22 +1,30 @@
 from storage import load_data, save_data
 from models import create_record
 
+
 record_list = load_data()
 
 
 def add_record(record_list, record_item):
     record_list.append(record_item)
-    
+
+
 def view_record_list(record_list):
     return record_list
 
-def update_record(record_list, record_id, new_name, new_value):
+
+def update_record(record_list, record_id, new_name, new_price, new_quantity, new_category, new_brand, new_status):
     for record_item in record_list:
         if record_item["id"] == record_id:
             record_item["name"] = new_name
-            record_item["value"] = new_value
+            record_item["price"] = new_price
+            record_item["quantity"] = new_quantity
+            record_item["category"] = new_category
+            record_item["brand"] = new_brand
+            record_item["status"] = new_status
             return True
     return False
+
 
 def delete_record(record_list, record_id):
     for record_item in record_list:
@@ -26,21 +34,32 @@ def delete_record(record_list, record_id):
     return False
 
 
-
-def create_new_record(record_id, name, value):
-    record_item =  create_record(record_id, name, value)
+def create_new_record(record_id, name, price, quantity, category, brand, status):
+    record_item = create_record(record_id, name, price, quantity, category, brand, status)
     add_record(record_list, record_item)
     save_data(record_list)
+
 
 def get_all_record_list():
     return view_record_list(record_list)
 
-def edit_record(record_id, new_name, new_value):
-    updated = update_record(record_list, record_id, new_name, new_value)
+
+def edit_record(record_id, new_name, new_price, new_quantity, new_category, new_brand, new_status):
+    updated = update_record(
+        record_list,
+        record_id,
+        new_name,
+        new_price,
+        new_quantity,
+        new_category,
+        new_brand,
+        new_status
+    )
 
     if updated:
         save_data(record_list)
     return updated
+
 
 def remove_record(record_id):
     deleted = delete_record(record_list, record_id)
